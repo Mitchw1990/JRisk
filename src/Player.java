88c939cd3d561e2748c9711bd46868558a8c7013
Mitchw1990 <mwarrenb@asu.edu>
2016-04-29 02:25:36 -0700

    changs to player class

--- src/Player.java
+++ src/Player.java
@@ -8,15 +8,22 @@ public class Player {
     private ArrayList<Territory> conqueredTerritories;
     private ArrayList<Continent> conqueredContinents;
     private String name;
+    private ArrayList<Die> playerDies;
 
-    public Player(String name){
+    public Player(String name)
+    {
         this.name = name;
+        conqueredContinents = new ArrayList<Territory>;
+        conqueredTerritories = new ArrayList<Territory>;
+        playerDies = new ArrayList<Die>;
+        playerDies.add(new Die());
+        playerDies.add(new Die());
+        playerDies.add(new Die());
     }
 
     public void addTerritory(Territory territory){
         conqueredTerritories.add(territory);
     }
-
     public void addContinent(Continent continent){
         conqueredContinents.add(continent);
     }
@@ -24,7 +31,6 @@ public class Player {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
